5e16a351a49b436935cd407bdb6b26c3862abf6e
streaker16 <abidaffa72@gmail.com>
2019-05-09 08:51:18 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import Main from './components/page/Main';
+import MyAppBar from "./components/MyAppBar";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                
+                <MyAppBar/> 
+                <Main/>
+                
+            </div>
+        );
+    }
+}
+
+export default App;
